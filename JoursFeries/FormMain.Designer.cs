e320381f17e2dd2e29a5a9c62aebed9d91d76663
Freddy Juhel <domaine75@gmail.com>
2014-07-20 19:05:40 +0200

    starting the solution

---
+++ JoursFeries/FormMain.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace JoursFeries
+{
+  partial class FormMain
+  {
+    /// <summary>
+    /// Variable nécessaire au concepteur.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Nettoyage des ressources utilisées.
+    /// </summary>
+    /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Code généré par le Concepteur Windows Form
+
+    /// <summary>
+    /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+    /// le contenu de cette méthode avec l'éditeur de code.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+      this.labelJourFerie = new System.Windows.Forms.Label();
+      this.comboBoxCountryList = new System.Windows.Forms.ComboBox();
+      this.labelCountry = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // dateTimePicker1
+      // 
+      this.dateTimePicker1.Location = new System.Drawing.Point(53, 140);
+      this.dateTimePicker1.Name = "dateTimePicker1";
+      this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+      this.dateTimePicker1.TabIndex = 0;
+      this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+      // 
+      // labelJourFerie
+      // 
+      this.labelJourFerie.AutoSize = true;
+      this.labelJourFerie.Location = new System.Drawing.Point(50, 106);
+      this.labelJourFerie.Name = "labelJourFerie";
+      this.labelJourFerie.Size = new System.Drawing.Size(61, 13);
+      this.labelJourFerie.TabIndex = 1;
+      this.labelJourFerie.Text = "Est-il férie ?";
+      // 
+      // comboBoxCountryList
+      // 
+      this.comboBoxCountryList.FormattingEnabled = true;
+      this.comboBoxCountryList.Location = new System.Drawing.Point(329, 138);
+      this.comboBoxCountryList.Name = "comboBoxCountryList";
+      this.comboBoxCountryList.Size = new System.Drawing.Size(121, 21);
+      this.comboBoxCountryList.TabIndex = 2;
+      // 
+      // labelCountry
+      // 
+      this.labelCountry.AutoSize = true;
+      this.labelCountry.Location = new System.Drawing.Point(273, 141);
+      this.labelCountry.Name = "labelCountry";
+      this.labelCountry.Size = new System.Drawing.Size(46, 13);
+      this.labelCountry.TabIndex = 3;
+      this.labelCountry.Text = "Country:";
+      // 
+      // FormMain
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(752, 474);
+      this.Controls.Add(this.labelCountry);
+      this.Controls.Add(this.comboBoxCountryList);
+      this.Controls.Add(this.labelJourFerie);
+      this.Controls.Add(this.dateTimePicker1);
+      this.Name = "FormMain";
+      this.ShowIcon = false;
+      this.Text = "Getting to know if a date is a bank holiday";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    private System.Windows.Forms.Label labelJourFerie;
+    private System.Windows.Forms.ComboBox comboBoxCountryList;
+    private System.Windows.Forms.Label labelCountry;
+  }
+}
+
---
+++ JoursFeries/FormMain.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+using Itenso.TimePeriod;
+
+
+namespace JoursFeries
+{
+  public partial class FormMain : Form
+  {
+    public FormMain()
+    {
+      InitializeComponent();
+    }
+
+    private TimeRange CurrentYearRange;
+    private const string CountryListFileName = @"ListOfCountries.xml";
+
+    private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+    {
+      labelJourFerie.Text = dateTimePicker1.Value.ToLongDateString() + " est férié ou pas";
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      CurrentYearRange = new TimeRange(new DateTime(DateTime.Now.Year, 1, 1), new DateTime(DateTime.Now.Year, 12, 31));
+      //loading country list to comboBoxCountryList
+      foreach (string countryName in LoadCountries(CountryListFileName))
+      {
+        comboBoxCountryList.Items.Add(countryName);
+      }
+
+      comboBoxCountryList.SelectedIndex = 0;
+
+    }
+
+    private static IEnumerable<string> LoadCountries(string filename)
+    {
+      if (!File.Exists(filename))
+      {
+        using (StreamWriter sw = new StreamWriter(filename))
+        {
+          sw.WriteLine(@"<?xml version=""1.0"" encoding=""utf-8"" ?>""");
+          sw.WriteLine(@"<countries>");
+          sw.WriteLine(@" <country>France</country>");
+          sw.WriteLine(@" <country>United States of America</country>");
+          sw.WriteLine(@" <country>Spain</country>");
+          sw.WriteLine(@" <country>Italy</country>");
+          sw.WriteLine(@" <country>Germany</country>");
+          sw.WriteLine(@" <country>Netherlands</country>");
+          sw.WriteLine(@" <country>United Kingdom</country>");
+          sw.WriteLine(@"</countries>");
+        }
+      }
+      List<string> result = new List<string>();
+      XmlTextReader reader = new XmlTextReader(filename);
+      
+      while (reader.Read())
+      {
+        if (reader.NodeType == XmlNodeType.Element && reader.Name == "country")
+        {
+          if (reader.HasAttributes)
+          {
+            result.Add(reader.GetAttribute("name")); 
+          }
+        }
+      }
+
+      //var xml = XDocument.Load(filename);
+      //if (xml.Root != null)
+      //{
+      //  var query = from c in xml.Root.Descendants("countries")
+      //              where  c.Name == "country"
+      //              //let xElement = c.Element("country")
+      //              //where xElement != null
+      //              //select c.Value;
+      //              select c.Value;
+
+      //  result.AddRange(query);
+      //}
+
+      return result.Count != 0 ? result : new List<string> { "France" };
+    }
+  }
+}
